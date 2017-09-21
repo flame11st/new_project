abf1d9ad436cd0530e9850223e2d76277d8ebea8
Sergey Tkachenko <Tkachenko@tpg.local>
2017-09-21 18:47:57 +0300

    Changes in templates

--- new_project/settings.py
+++ new_project/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,4 +120,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, ".")
+MEDIA_URL = '/media/'
--- new_project/urls.py
+++ new_project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^routes/', include('routes.urls')),
-	url(r'^points/', include('points.urls'))
-]
+	url(r'^points/', include('points.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
